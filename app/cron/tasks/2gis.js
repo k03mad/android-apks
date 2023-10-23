1827957f695bf5034b8bf107d2d93fc485750b70
k03mad <k03.mad@gmail.com>
2023-10-24 00:10:47 +0300

    update 2gis

--- app/cron/tasks/2gis.js
+++ app/cron/tasks/2gis.js
@@ -1,18 +1,10 @@
-import {request} from '@k03mad/request';
-
 import {cleanFolderDownloadApk} from '../../../utils/aria.js';
 import {getCurrentFilename} from '../../../utils/files.js';
 import config from '../../server/config.js';
 
 const APK_DIR = `${config.static.apk}/${getCurrentFilename(import.meta.url)}`;
 
-const REQUEST_URL = 'https://apk.2gis.ru/';
-const RESPONSE_ELEMENT_RE = /clickLink":"(.+?)"/;
+const APK_URL = 'https://disk.2gis.com/android/Latest/2GIS.apk';
 
 /** */
-export default async () => {
-    const {body} = await request(REQUEST_URL);
-
-    const href = body.match(RESPONSE_ELEMENT_RE)[1];
-    await cleanFolderDownloadApk(APK_DIR, href);
-};
+export default () => cleanFolderDownloadApk(APK_DIR, APK_URL);
