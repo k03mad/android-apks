5ce2712b136a8dd9f3dda450899cb4199c703b86
k03mad <k03.mad@gmail.com>
2023-10-24 00:15:24 +0300

    update deps

--- app/cron/tasks/2gis.js
+++ app/cron/tasks/2gis.js
@@ -6,5 +6,7 @@ const APK_DIR = `${config.static.apk}/${getCurrentFilename(import.meta.url)}`;
 
 const APK_URL = 'https://disk.2gis.com/android/Latest/2GIS.apk';
 
-/** */
+/**
+ * @returns {Promise<void>}
+ */
 export default () => cleanFolderDownloadApk(APK_DIR, APK_URL);
