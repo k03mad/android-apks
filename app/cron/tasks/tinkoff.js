44aa127d5f2d39201eddab8f28fd916166a6b806
k03mad <k03.mad@gmail.com>
2023-10-24 00:35:51 +0300

    update folders

--- app/cron/tasks/tinkoff.js
+++ app/cron/tasks/tinkoff.js
@@ -6,9 +6,15 @@ import config from '../../server/config.js';
 
 const APK_DIR = `${config.static.apk}/${getCurrentFilename(import.meta.url)}`;
 
-const DOWNLOAD_APPS_BY_LINK_INCLUDES = [
-    'Tinkoff_Invest',
-    'tinkoff-bank',
+const DOWNLOAD_APPS = [
+    {
+        hrefIncludes: 'Tinkoff_Invest',
+        toFolder: 'tinkoff-invest',
+    },
+    {
+        hrefIncludes: 'tinkoff-bank',
+        toFolder: 'tinkoff-bank',
+    },
 ];
 
 const REQUEST_URL = 'https://www.tinkoff.ru/apps/';
@@ -28,9 +34,12 @@ export default async () => {
     const hrefs = body.match(RESPONSE_ELEMENT_RE);
 
     await Promise.all([...new Set(hrefs)].map(async href => {
-        for (const app of DOWNLOAD_APPS_BY_LINK_INCLUDES) {
-            if (href.includes(app)) {
-                await cleanFolderDownloadApk(`${APK_DIR}/${app}`, href);
+        for (const app of DOWNLOAD_APPS) {
+            if (href.includes(app.hrefIncludes)) {
+                await cleanFolderDownloadApk([
+                    APK_DIR,
+                    app.toFolder,
+                ].join('/'), href);
             }
         }
     }));
