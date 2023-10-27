ec5fc83d7dc459cee2d17d1f7219df3370211c40
k03mad <k03.mad@gmail.com>
2023-10-27 16:23:07 +0300

    css fix, add debug

--- utils/aria.js
+++ utils/aria.js
@@ -1,7 +1,11 @@
 import fs from 'node:fs/promises';
 
+import _debug from 'debug';
+
 import {getUa} from './request.js';
 import {run} from './shell.js';
+
+const debug = _debug('mad:aria');
 
 const getAriaArgs = uaType => [
     '--continue=true',
@@ -20,7 +24,11 @@ const getAriaArgs = uaType => [
  */
 export const download = async (dir, url, opts = {}) => {
     await fs.mkdir(dir, {recursive: true});
+
+    const cmd = `cd ${dir} && aria2c ${getAriaArgs(opts.ua)} ${url}`;
+    debug(cmd);
     const output = await run(`cd ${dir} && aria2c ${getAriaArgs(opts.ua)} ${url}`);
+    debug(output);
 
     const apkPath = output?.match(/\/(.+)\.apk/)?.[1];
     return apkPath;
