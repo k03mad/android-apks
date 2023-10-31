fc4c49782f2216f47061eeb8e169ec77523e0790
k03mad <k03.mad@gmail.com>
2023-11-01 01:37:15 +0300

    add tap store

--- app/cron/providers/store.js
+++ app/cron/providers/store.js
@@ -1,6 +1,7 @@
 /** */
 export default () => [
     {link: 'https://appgallery.cloud.huawei.com/appdl/C27162'},
+    {link: 'https://d.tap.io/latest'},
     {link: 'https://f-droid.org/F-Droid.apk'},
     {link: 'https://www.rustore.ru/download'},
 ];
