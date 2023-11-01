e83d04e4fbff6f2529e47daf01ddbc5da42e6a75
k03mad <k03.mad@gmail.com>
2023-11-01 22:59:10 +0300

    remove uptd

--- app/cron/providers/store.js
+++ app/cron/providers/store.js
@@ -2,7 +2,6 @@
 export default () => [
     {link: 'https://appgallery.cloud.huawei.com/appdl/C27162'},
     {link: 'https://d.tap.io/latest'},
-    {link: 'https://dw16.uptodown.com/dwn/t0xe6Is-ZK9uldbCQix_hsYW_xKgPYHSyYPmNun_EzUjCWOATLpAVVgF7mv1lzKqiLv1L3LGvoWxjXwbqrwchHNSl0KDivjF5uJpi2v12IIT2S6YWe4DqojXX-0vKyWa/3WeQo_rMInTh24_oWUeAzuKm-ixgr9G8GKnvGswHdqLgOlTOSHjmkI5wfMN9Q6hKoKOBQtv0855p1YXkxv7x9PLDufn8B5aPWBPM0Eiv4vdpBMTZiVLJ0EEvMLO6soih/yp18bkmihVJevpbaurgxbHJhq7HezOF_H8jp6QasA-0-YjAkmshRKFzMUlQtX7ppDv3ixKhSEeK3TwVBoOQo6Yf5nI2gd3J3ho-UQLD9Rgw=/'},
     {link: 'https://f-droid.org/F-Droid.apk'},
     {link: 'https://www.rustore.ru/download'},
 ];
