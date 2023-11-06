f7f86595031d83c3721c9151f58a6f6611d0a8bd
k03mad <k03.mad@gmail.com>
2023-11-06 18:16:34 +0300

    update rvx

--- app/cron/providers/github_revanced.js
+++ app/cron/providers/github_revanced.js
@@ -1,12 +1,11 @@
-import {getApkFromGhUsers} from './shared/github-release.js';
+import {getApkFromGhRepos} from './shared/github-release.js';
 
-const users = [
-    {name: 'inotia00'},
-    {name: 'revanced'},
-    {name: 'NoName-exe'},
+const repos = [
+    {name: 'NoName-exe/revanced-extended', re: {include: /arm64|all/}},
+    {name: 'inotia00/VancedMicroG'},
 ];
 
 /**
  * @returns {Promise<Array<{link: string}>>}
  */
-export default () => getApkFromGhUsers(users);
+export default () => getApkFromGhRepos(repos);
