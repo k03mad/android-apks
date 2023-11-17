b132b0a3a9a4131451cac7e342e3587d13750b51
k03mad <k03.mad@gmail.com>
2023-11-18 01:57:40 +0300

    microg up

--- app/cron/providers/github_microg.js
+++ app/cron/providers/github_microg.js
@@ -3,7 +3,6 @@ import {getApkFromGhRepos} from './helpers/github.js';
 const repos = [
     {name: 'inotia00/mMicroG', re: {include: /arm64/}},
     {name: 'inotia00/VancedMicroG'},
-    {name: 'TeamVanced/VancedMicroG'},
     {name: 'microg/GmsCore', re: {include: /gms/, exclude: /-(lh|hw)/}},
 ];
 
