2e1551c586d3024fac5d688a2c91dc97395fc445
k03mad <k03.mad@gmail.com>
2023-11-17 22:03:12 +0300

    update github

--- app/cron/providers/github.js
+++ app/cron/providers/github.js
@@ -1,14 +1,22 @@
-import {getApkFromGhRepos} from './helpers/github.js';
+import {getApkFromGhOrgs, getApkFromGhUsers} from './helpers/github.js';
 
 /** */
-export default () => getApkFromGhRepos([
-    {name: 'EtchDroid/EtchDroid'},
-    {name: 'imranr98/obtainium', re: {include: /arm64/}},
-    {name: 'kyujin-cho/pixel-volte-patch'},
-    {name: 'massivemadness/Squircle-CE', re: {include: /fdroid/}},
-    {name: 'MuntashirAkon/AppManager'},
-    {name: 'organicmaps/organicmaps'},
-    {name: 'RikkaApps/Shizuku'},
-    {name: 'streetcomplete/StreetComplete'},
-    {name: 'termux/termux-app', re: {include: /arm64/}},
-]);
+export default async () => {
+    const links = await Promise.all([
+        getApkFromGhUsers([
+            {name: 'MuntashirAkon'},
+            {name: 'imranr98'},
+            {name: 'kyujin-cho'},
+            {name: 'massivemadness'},
+        ]),
+        getApkFromGhOrgs([
+            {name: 'EtchDroid'},
+            {name: 'organicmaps'},
+            {name: 'RikkaApps'},
+            {name: 'streetcomplete'},
+            {name: 'termux'},
+        ]),
+    ]);
+
+    return links.flat();
+};
