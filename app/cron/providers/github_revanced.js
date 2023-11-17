3950b8eb4b7cf8205efa17be9f0eaf1c0cfa50a1
k03mad <k03.mad@gmail.com>
2023-11-18 01:52:33 +0300

    rev up

--- app/cron/providers/github_revanced.js
+++ app/cron/providers/github_revanced.js
@@ -1,6 +1,6 @@
 import {getApkFromGhUsers} from './helpers/github.js';
 
-const users = [{name: 'NoName-exe', re: {include: /all|arm64/}}];
+const users = [{name: 'NoName-exe', re: {include: /mnml/}}];
 
 /** */
 export default () => getApkFromGhUsers(users);
