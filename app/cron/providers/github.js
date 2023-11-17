ed1b3b950198267af68a003aad75a23f8ba64f28
k03mad <k03.mad@gmail.com>
2023-11-17 23:35:02 +0300

    add apps

--- app/cron/providers/github.js
+++ app/cron/providers/github.js
@@ -2,11 +2,15 @@ import {getApkFromGhRepos} from './helpers/github.js';
 
 /** */
 export default () => getApkFromGhRepos([
+    {name: 'Chooloo/koler'},
     {name: 'EtchDroid/EtchDroid'},
     {name: 'imranr98/obtainium', re: {include: /arm64/}},
     {name: 'kyujin-cho/pixel-volte-patch'},
+    {name: 'M66B/NetGuard'},
     {name: 'massivemadness/Squircle-CE', re: {include: /fdroid/}},
     {name: 'MuntashirAkon/AppManager'},
     {name: 'organicmaps/organicmaps'},
+    {name: 'proninyaroslav/libretorrent', re: {include: /arm64/}},
     {name: 'RikkaApps/Shizuku'},
+    {name: 'schwabe/ics-openvpn'},
 ]);
--- app/cron/providers/github_youtube.js
+++ app/cron/providers/github_youtube.js
@@ -3,6 +3,7 @@ import {getApkFromGhRepos} from './helpers/github.js';
 const repos = [
     {name: 'inotia00/mMicroG', re: {include: /arm64/}},
     {name: 'inotia00/VancedMicroG'},
+    {name: 'libre-tube/LibreTube', re: {include: /arm64/}},
     {name: 'NoName-exe/revanced-extended', re: {include: /all/}},
     {name: 'polymorphicshade/NewPipe'},
     {name: 'revanced/revanced-manager'},
