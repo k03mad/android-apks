0a58d998e9fef3a355322045cc2f9622ac60f5c8
k03mad <k03.mad@gmail.com>
2023-11-19 20:52:38 +0300

    remove monet

--- app/cron/providers/github_termux_monet.js
+++
@@ -1,6 +0,0 @@
-import {getApkFromGhUsers} from './helpers/github.js';
-
-const orgs = [{name: 'HardcodedCat', re: {exclude: /universal|x86|v7a/}}];
-
-/** */
-export default () => getApkFromGhUsers(orgs);
