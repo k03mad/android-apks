d323107ef19e01423b7bbcde6852c03b39e5d8ee
Kirill Molchanov <k03.mad@gmail.com>
2023-11-19 20:49:40 +0300

    Update github_revanced.js

--- app/cron/providers/github_revanced.js
+++ app/cron/providers/github_revanced.js
@@ -1,6 +1,6 @@
 import {getApkFromGhUsers} from './helpers/github.js';
 
-const users = [{name: 'NoName-exe', re: {include: /mnml/}}];
+const users = [{name: 'NoName-exe', re: {include: /mnml/, exclude: /youtube-.+revanced-mnml/}}];
 
 /** */
 export default () => getApkFromGhUsers(users);
