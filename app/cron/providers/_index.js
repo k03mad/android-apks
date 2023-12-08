51473b37aef73699c925046b5fe49cd47498e802
k03mad <k03.mad@gmail.com>
2023-12-08 16:22:30 +0300

    unskip

--- app/cron/providers/_index.js
+++ app/cron/providers/_index.js
@@ -1,13 +1,13 @@
 /* eslint-disable camelcase */
 
 export {default as common} from './common.js';
-// export {default as common_bank} from './common_bank.js';
-// export {default as common_bank_alfa} from './common_bank_alfa.js';
-// export {default as common_bank_sber} from './common_bank_sber.js';
-// export {default as common_bank_tinkoff} from './common_bank_tinkoff.js';
-// export {default as common_media} from './common_media.js';
-// export {default as common_mobile} from './common_mobile.js';
-// export {default as common_store} from './common_store.js';
+export {default as common_bank} from './common_bank.js';
+export {default as common_bank_alfa} from './common_bank_alfa.js';
+export {default as common_bank_sber} from './common_bank_sber.js';
+export {default as common_bank_tinkoff} from './common_bank_tinkoff.js';
+export {default as common_media} from './common_media.js';
+export {default as common_mobile} from './common_mobile.js';
+export {default as common_store} from './common_store.js';
 export {default as fdroid} from './fdroid.js';
 export {default as github} from './github.js';
 export {default as github_darkmoonight} from './github_darkmoonight.js';
