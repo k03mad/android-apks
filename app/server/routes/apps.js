e60bd9e5277fe5e33cf889dd2e6e8b4a71ffa3bb
k03mad <k03.mad@gmail.com>
2023-12-17 04:58:47 +0300

    remove json cache

--- app/server/routes/apps.js
+++ app/server/routes/apps.js
@@ -8,22 +8,13 @@ import serverConfig from '../../server/config.js';
 const router = express.Router();
 const userAgents = new Set();
 
-let modTime, parsedData;
-
 /**
  * @param {object} req
  * @param {object} [req.headers]
  */
 const getPageData = async req => {
-    const {mtime} = await fs.stat(cronConfig.json.file);
-
-    if (String(mtime) !== String(modTime)) {
-        const dataFile = await fs.readFile(cronConfig.json.file);
-
-        parsedData = JSON.parse(dataFile);
-        // eslint-disable-next-line require-atomic-updates
-        modTime = mtime;
-    }
+    const dataFile = await fs.readFile(cronConfig.json.file);
+    const parsedData = JSON.parse(dataFile);
 
     const ua = req.headers?.['user-agent'];
 
