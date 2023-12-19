df4f73437eea67c47c3d42392000ee7348d79d6e
k03mad <k03.mad@gmail.com>
2023-12-20 00:27:04 +0300

    add ru.alfastrah.app

--- app/cron/providers/common_bank_alfa.js
+++ app/cron/providers/common_bank_alfa.js
@@ -1,6 +1,18 @@
-/** */
-export default () => [
+import {getApkFromAgIds} from './helpers/huawei.js';
+
+const direct = [
     {link: 'https://alfamobile-web.servicecdn.ru/latest/alfa-bank.apk'},
     {link: 'https://alfainvest-web.servicecdn.ru/invest-android/latest/a-investments.apk'},
     {link: 'https://alfabusinessmobile.servicecdn.ru/abm/latest/ABM-release.apk'},
 ].map(elem => ({...elem, homepage: 'https://alfabank.ru/apps/'}));
+
+const agIds = [
+    // ru.alfastrah.app
+    'C103154181',
+];
+
+/** */
+export default () => {
+    const ag = getApkFromAgIds(agIds);
+    return [direct, ag].flat();
+};
