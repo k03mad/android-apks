bf6e8be6411cb92c630a26c4cf871eff5db34408
k03mad <k03.mad@gmail.com>
2023-12-29 13:17:24 +0300

    update apps

--- app/cron/providers/common.js
+++ app/cron/providers/common.js
@@ -8,6 +8,26 @@ const direct = [
         homepage: 'https://apk.2gis.ru/',
         opts: {ua: 'curl', proxy: true},
     },
+    {
+        link: 'https://kpdl.cc/k.apk',
+        homepage: 'https://kpdl.cc/faq.html',
+    },
+    {
+        link: 'https://static.yota.ru/upload/apps/ru.yota.android.apk',
+        homepage: 'https://app.yota.ru/',
+    },
+    {
+        link: 'https://lk.megafon.ru/download/apk',
+        homepage: 'https://lc.megafon.ru/help/lk/',
+    },
+    {
+        link: 'https://d.tap.io/latest',
+        homepage: 'https://www.taptap.io/mobile',
+    },
+    {
+        link: 'https://www.rustore.ru/download',
+        homepage: 'https://www.rustore.ru/instruction.html',
+    },
 ];
 
 const parse = [
@@ -15,10 +35,6 @@ const parse = [
         homepage: 'https://download.wireguard.com/android-client/',
         re: /href="(.+\.apk)"/,
         relative: true,
-    },
-    {
-        homepage: 'https://televizo.net/',
-        re: /href="(.+)">APK \(ALL DEVICES\)/,
     },
     {
         homepage: 'https://питание.дети/mobile',
@@ -29,11 +45,24 @@ const parse = [
         re: /href="(https:\/\/scontent.+)" data.+Android_PackageInstaller/,
         replace: {from: /&amp;/g, to: '&'},
     },
+    {
+        homepage: 'https://get.videolan.org/vlc-android/last/',
+        re: /href="(.+arm64.+\.apk)"/,
+        opts: {ua: 'curl'},
+        relative: true,
+    },
+    {
+        homepage: 'https://televizo.net/',
+        re: /href="(.+)">APK \(ALL DEVICES\)/,
+    },
 ];
 
 const apApps = [{path: 'mi-home/com.xiaomi.smarthome'}];
 
-const agApps = [{name: 'РЖД', id: 'C101979029'}];
+const agApps = [
+    {name: 'РЖД', id: 'C101979029'},
+    {name: 'AppGallery', id: 'C27162'},
+];
 
 /** */
 export default async () => {
