2c39501f9d4cfc8941a8bd55de9c6a443d892618
Kirill Molchanov <k03.mad@gmail.com>
2024-01-02 23:54:41 +0300

    Update _index.js

--- app/cron/providers/_index.js
+++ app/cron/providers/_index.js
@@ -5,7 +5,6 @@ export {default as apkpure} from './apkpure.js';
 export {default as appgallery} from './appgallery.js';
 export {default as fdroid} from './fdroid.js';
 export {default as github_fossify} from './github_fossify.js';
-export {default as github_revanced} from './github_revanced.js';
 export {default as github_termux} from './github_termux.js';
 export {default as github_youtube} from './github_youtube.js';
 export {default as github} from './github.js';
