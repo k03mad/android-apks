953fae10a43d9c53024cf687c310503d926daffc
k03mad <k03.mad@gmail.com>
2024-01-03 00:05:05 +0300

    remove termux provider

--- app/cron/providers/github_termux.js
+++
@@ -1,6 +0,0 @@
-import {getApkFromGhOrgs} from './helpers/github.js';
-
-const orgs = [{name: 'termux', re: {exclude: /nightly|universal|v7a|x86/}}];
-
-/** */
-export default () => getApkFromGhOrgs(orgs);
