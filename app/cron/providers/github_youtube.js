014df82b4d43f5fa2cac1515ceb0650e659258e5
Kirill Molchanov <k03.mad@gmail.com>
2024-01-02 23:53:01 +0300

    Update github_youtube.js

--- app/cron/providers/github_youtube.js
+++ app/cron/providers/github_youtube.js
@@ -1,7 +1,6 @@
 import {getApkFromGhRepos} from './helpers/github.js';
 
 const repos = [
-    {name: 'libre-tube/LibreTube', re: {include: /arm64/}},
     {name: 'polymorphicshade/NewPipe'},
     {name: 'TeamNewPipe/NewPipe'},
     {name: 'yuliskov/SmartTube', re: {include: /arm64/}},
