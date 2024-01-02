255fed6a5ce8e213f07622f0834d762f3db82a84
k03mad <k03.mad@gmail.com>
2024-01-02 23:56:28 +0300

    remove rev provider

--- app/cron/providers/github_revanced.js
+++
@@ -1,7 +0,0 @@
-import {getApkFromGhRepos} from './helpers/github.js';
-
-/** */
-export default () => getApkFromGhRepos([
-    {name: 'inotia00/VancedMicroG'},
-    {name: 'NoName-exe/revanced-extended-mnml'},
-]);
