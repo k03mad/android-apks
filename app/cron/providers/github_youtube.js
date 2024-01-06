22f131daa7182e43498a27536c6cf0d22d908e4e
k03mad <k03.mad@gmail.com>
2024-01-06 18:51:37 +0300

    pretty

--- app/cron/providers/github_youtube.js
+++ app/cron/providers/github_youtube.js
@@ -1,12 +1,10 @@
 import {getApkFromGhRepos} from './helpers/github.js';
 
-const repos = [
+/** */
+export default () => getApkFromGhRepos([
     {name: 'inotia00/VancedMicroG'},
     {name: 'NoName-exe/revanced-extended-mnml', re: {exclude: /reddit|youtube-music/}},
     {name: 'polymorphicshade/NewPipe'},
     {name: 'TeamNewPipe/NewPipe'},
     {name: 'yuliskov/SmartTube', skipPrerelease: true, re: {include: /arm64/}},
-];
-
-/** */
-export default () => getApkFromGhRepos(repos);
+]);
