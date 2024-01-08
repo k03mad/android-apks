2891a6615ebdff310312550fb2343971366414c2
k03mad <k03.mad@gmail.com>
2024-01-08 03:12:44 +0300

    update deps

--- app/cron/providers/_index.js
+++ app/cron/providers/_index.js
@@ -3,9 +3,9 @@
 export {default as _4pda} from './4pda.js';
 export {default as apkpure} from './apkpure.js';
 export {default as fdroid} from './fdroid.js';
-export {default as github_youtube} from './github_youtube.js';
 export {default as github} from './github.js';
+export {default as github_youtube} from './github_youtube.js';
 export {default as gitlab} from './gitlab.js';
 export {default as various_direct} from './various_direct.js';
-export {default as various_parse_tinkoff} from './various_parse_tinkoff.js';
 export {default as various_parse} from './various_parse.js';
+export {default as various_parse_tinkoff} from './various_parse_tinkoff.js';
