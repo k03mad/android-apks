72d154fc2343e3c0ee0a5d8d9ff3e7d2665906f6
k03mad <k03.mad@gmail.com>
2024-01-10 19:41:46 +0300

    pretty

--- utils/logs.js
+++ utils/logs.js
@@ -1,17 +1,20 @@
 /* eslint-disable no-console */
 
+import {getDateYMDHMS} from '@k03mad/simple-date';
+
 import {convertToArray} from './array.js';
 
 /**
- * @param {string|string[]} msg
+ * @param {any|any[]} msg
  * @returns {void}
  */
 export const log = msg => convertToArray(msg)
     .forEach(elem => console.log(elem));
 
 /**
- * @param {string|string[]} msg
+ * @param {any|any[]} msg
  * @returns {void}
  */
-export const logError = msg => convertToArray(msg)
+export const logError = msg => [getDateYMDHMS(), convertToArray(msg)]
+    .flat()
     .forEach(elem => console.error(elem));
