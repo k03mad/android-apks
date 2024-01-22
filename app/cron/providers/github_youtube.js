fe8c913cb9f6a048574f5eef3258243bfa09fb92
k03mad <k03.mad@gmail.com>
2024-01-22 20:47:56 +0300

    update youtube

--- app/cron/providers/github_youtube.js
+++ app/cron/providers/github_youtube.js
@@ -4,6 +4,6 @@ import {getApkFromGhRepos} from './helpers/github.js';
 export default () => getApkFromGhRepos([
     {name: 'inotia00/VancedMicroG'},
     {name: 'NoName-exe/revanced-extended-mnml', re: {exclude: /reddit|youtube-music/}},
-    {name: 'polymorphicshade/NewPipe'},
+    {name: 'polymorphicshade/Tubular'},
     {name: 'yuliskov/SmartTube', skipPrerelease: true, re: {include: /arm64/}},
 ]);
