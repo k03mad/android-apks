5dd216e2349e2dfe396813c0ba6d65f5e79a4b28
k03mad <k03.mad@gmail.com>
2024-01-24 02:37:36 +0300

    merge github

--- app/cron/providers/github_youtube.js
+++
@@ -1,9 +0,0 @@
-import {getApkFromGhRepos} from './helpers/github.js';
-
-/** */
-export default () => getApkFromGhRepos([
-    {name: 'inotia00/VancedMicroG'},
-    {name: 'NoName-exe/revanced-extended-mnml', filter: {exclude: /reddit|youtube-music/}},
-    {name: 'polymorphicshade/Tubular'},
-    {name: 'yuliskov/SmartTube', skipPrerelease: true, filter: {include: /arm64/}},
-]);
