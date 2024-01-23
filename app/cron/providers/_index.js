1d08aa5bb3b7612e8d62a9c557c55fb4e39632d2
k03mad <k03.mad@gmail.com>
2024-01-23 19:59:12 +0300

    update naming

--- app/cron/providers/_index.js
+++ app/cron/providers/_index.js
@@ -2,11 +2,11 @@
 
 export {default as _4pda} from './4pda.js';
 export {default as apkpure} from './apkpure.js';
+export {default as direct} from './direct.js';
 export {default as fdroid} from './fdroid.js';
 export {default as github} from './github.js';
 export {default as github_youtube} from './github_youtube.js';
 export {default as gitlab} from './gitlab.js';
-export {default as strannik} from './strannik.js';
-export {default as various_direct} from './various_direct.js';
-export {default as various_parse} from './various_parse.js';
-export {default as various_parse_tinkoff} from './various_parse_tinkoff.js';
+export {default as parse} from './parse.js';
+export {default as parse_strannik} from './parse_strannik.js';
+export {default as parse_tinkoff} from './parse_tinkoff.js';
