f6f99f5d853afcb9b68aa4ef8df96681ef20cae7
Kirill Molchanov <k03.mad@gmail.com>
2024-02-18 16:09:00 +0300

    Update 5play.js

--- app/cron/providers/5play.js
+++ app/cron/providers/5play.js
@@ -1,4 +1,7 @@
 import {getApkFrom5Play} from './helpers/5play.js';
 
 /** */
-export default () => getApkFrom5Play([{name: '4159-vampire-survivors'}]);
+export default () => getApkFrom5Play([
+    {name: '4159-vampire-survivors'},
+    {name: '11163-20-minutes-till-dawn'},
+]);
