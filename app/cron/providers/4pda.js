8f1e6905a6460597619605639727bedb0fbe8f7e
Kirill Molchanov <k03.mad@gmail.com>
2024-03-16 02:08:29 +0300

    Update 4pda.js

--- app/cron/providers/4pda.js
+++ app/cron/providers/4pda.js
@@ -3,7 +3,6 @@ import {getApkFrom4Pda} from './helpers/4pda.js';
 /** */
 export default () => getApkFrom4Pda([
     {name: '4PDA', showtopic: 673_755},
-    {name: 'Чей номер', showtopic: 1_071_145},
     {name: 'AnyBalance', showtopic: 300_836},
     {name: 'App&Game 4PDA', showtopic: 275_433},
     {name: 'Brotato Mobile', showtopic: 1_059_651},
