03480c174ea5f135dc8a0f33593e43923cb8408e
k03mad <k03.mad@gmail.com>
2024-03-28 23:46:35 +0300

    remove 5play

--- app/cron/providers/5play.js
+++
@@ -1,7 +0,0 @@
-import {getApkFrom5Play} from './helpers/5play.js';
-
-/** */
-export default () => getApkFrom5Play([
-    {name: '4159-vampire-survivors'},
-    {name: '11163-20-minutes-till-dawn'},
-]);
--- app/cron/providers/_index.js
+++ app/cron/providers/_index.js
@@ -1,5 +1,4 @@
 export {default as _4pda} from './4pda.js';
-export {default as _5play} from './5play.js';
 export {default as apkpure} from './apkpure.js';
 export {default as direct} from './direct.js';
 export {default as fdroid} from './fdroid.js';
