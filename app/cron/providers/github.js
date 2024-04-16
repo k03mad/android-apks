298978259ab1f505ac4ca7833d262942e414f954
k03mad <k03.mad@gmail.com>
2024-04-16 21:09:08 +0300

    update syncthing

--- app/cron/providers/github.js
+++ app/cron/providers/github.js
@@ -4,12 +4,14 @@ import {getApkFromGhRepos} from './helpers/github.js';
 export default () => getApkFromGhRepos([
     {name: 'AdguardTeam/ContentBlocker'},
     {name: 'Aefyr/SAI'},
+    {name: 'Catfriend1/syncthing-android', filter: {exclude: /fdroid|gplay/}},
     {name: 'FossifyOrg/Gallery'},
     {name: 'imranr98/obtainium', filter: {include: /arm64/, exclude: /fdroid/}},
     {name: 'kyujin-cho/pixel-volte-patch'},
     {name: 'lampa-app/LAMPA'},
     {name: 'massivemadness/Squircle-CE', filter: {include: /fdroid/}},
     {name: 'organicmaps/organicmaps'},
+    {name: 'polymorphicshade/Tubular'},
     {name: 'RikkaApps/Shizuku'},
     {name: 'streetcomplete/StreetComplete'},
     {name: 'termux/termux-app', filter: {include: /arm64/}},
@@ -17,6 +19,4 @@ export default () => getApkFromGhRepos([
     {name: 'tytydraco/PDNSQS', filter: {exclude: /unsigned/}},
     {name: 'YouROK/TorrServe'},
     {name: 'yuliskov/SmartTube', skipPrerelease: true, filter: {include: /arm64/}},
-    {name: 'polymorphicshade/Tubular'},
-    {name: 'Catfriend1/syncthing-android', filter: {exclude: /gplay/}},
 ]);
