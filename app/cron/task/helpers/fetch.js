331a0a5b580d376e27c6063e8aca327da3577f4f
k03mad <k03.mad@gmail.com>
2024-04-16 23:36:12 +0300

    add rename debug

--- app/cron/task/helpers/fetch.js
+++ app/cron/task/helpers/fetch.js
@@ -2,6 +2,7 @@ import fs from 'node:fs/promises';
 import path from 'node:path';
 
 import {logError} from '@k03mad/simple-log';
+import _debug from 'debug';
 
 import {getApkFileInfo} from '../../../../utils/aapt.js';
 import {download} from '../../../../utils/aria.js';
@@ -9,6 +10,8 @@ import {convertToArray} from '../../../../utils/array.js';
 import {retry} from '../../../../utils/promise.js';
 import serverConfig from '../../../server/config.js';
 import cronConfig from '../../config.js';
+
+const debug = _debug('mad:fetch');
 
 /**
  * @param {object} providers
@@ -51,6 +54,7 @@ export const downloadApkFile = async ({homepage, link, opts}) => {
 
     const renamedFilePath = path.join(...downloadedApkPathSplit, `${info.pkg}_${info.version}.apk`);
     await fs.rename(downloadedApkPath, renamedFilePath);
+    debug.extend('rename')('%o', renamedFilePath);
 
     const relativeDownloadLink = renamedFilePath.replace(serverConfig.static.root, '');
 
