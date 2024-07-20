4b3978e376997e9fb29482910cf29ab32bea2e95
Kirill Molchanov <k03.mad@gmail.com>
2024-07-20 11:07:12 +0300

    Update apkpure.js

--- app/cron/providers/apkpure.js
+++ app/cron/providers/apkpure.js
@@ -1,4 +1,7 @@
 import {getApkFromAp} from './helpers/apkpure.js';
 
 /** */
-export default () => getApkFromAp([{name: 'mi-home/com.xiaomi.smarthome'}]);
+export default () => getApkFromAp([
+    {name: 'mi-home/com.xiaomi.smarthome'},
+    {name: 'x-plore-file-manager/com.lonelycatgames.Xplore'},
+]);
