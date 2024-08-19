e034b66d1a593ca2bece053256728fce1da2dd42
Kirill Molchanov <k03.mad@gmail.com>
2024-08-19 23:42:02 +0300

    Update github_youtube.js

--- app/cron/providers/github_youtube.js
+++ app/cron/providers/github_youtube.js
@@ -4,7 +4,7 @@ import {getApkFromGhRepos} from './helpers/github.js';
 export default () => getApkFromGhRepos([
     {name: 'NoName-exe/revanced', filter: {include: /youtube-revanced/}},
     {name: 'polymorphicshade/Tubular'},
-    {name: 'revanced-apks/build-apps', filter: {include: /youtube-revanced-extended/}},
+    {name: 'revanced-apks/build-apps', filter: {include: /(music|youtube)-revanced-extended/}},
     {name: 'ReVanced/GmsCore', filter: {exclude: /-hw-/}},
     {name: 'yuliskov/SmartTube', skipPrerelease: true, filter: {include: /arm64/}},
 ]);
