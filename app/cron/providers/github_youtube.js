ae32d3b3689b7001bdab0ca55551c6a7c1268ddc
k03mad <k03.mad@gmail.com>
2024-09-11 10:57:16 +0300

    fix rvx

--- app/cron/providers/github_youtube.js
+++ app/cron/providers/github_youtube.js
@@ -4,7 +4,7 @@ import {getApkFromGhRepos} from './helpers/github.js';
 export default () => getApkFromGhRepos([
     {name: 'NoName-exe/revanced', filter: {include: /youtube-revanced/}},
     {name: 'polymorphicshade/Tubular'},
-    {name: 'revanced-apks/build-apps', filter: {include: /(music|youtube)-revanced-extended/}},
+    {name: 'revanced-apks/build-apps', filter: {include: /(music|youtube)-revanced-extended-.+-(all|arm64)/}},
     {name: 'ReVanced/GmsCore', filter: {exclude: /-hw-/}},
     {name: 'yuliskov/SmartTube', skipPrerelease: true, filter: {include: /arm64/}},
 ]);
