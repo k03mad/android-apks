4b5bf147ead69887552b114d9982faedefd3391e
k03mad <k03.mad@gmail.com>
2024-10-14 15:18:54 +0300

    remove cat

--- app/cron/providers/parse.js
+++ app/cron/providers/parse.js
@@ -73,4 +73,49 @@ export default () => getApkFromParse([
             selector: '[onclick*="mp_apk"]',
         },
     },
+    {
+        page: 'https://get.videolan.org/vlc-android/last/',
+        opts: {
+            ua: 'curl',
+        },
+        href: {
+            re: /href="(.+arm64.+\.apk)"/,
+            relative: true,
+        },
+    },
+    {
+        page: 'https://kpdl.link/faq.html',
+        href: {
+            re: /href="(.+\.apk)"/,
+        },
+    },
+    {
+        page: 'https://televizo.net/',
+        href: {
+            text: 'DOWNLOAD APK FILE',
+        },
+    },
+    {
+        page: 'https://android.zona.pub/download.html',
+        href: {
+            text: 'Android 5 и выше',
+            replace: {
+                from: '//apk',
+                to: 'https://apkw',
+            },
+        },
+    },
+    {
+        page: 'https://www.tbank.ru/apps/android-invest/',
+        href: {selector: '[href$=".apk"]'},
+    },
+    {
+        page: 'https://www.tbank.ru/apps/android-mobile/',
+        href: {selector: '[href$=".apk"]'},
+    },
+    {
+        page: 'https://www.tbank.ru/apps/android-bank/',
+        intermediate: {selector: '[href*="download"]'},
+        href: {selector: '[href$=".apk"]'},
+    },
 ]);
